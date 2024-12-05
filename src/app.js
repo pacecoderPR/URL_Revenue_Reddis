b27104b8d5cd1f907aee381d5da7130c6072c842
pritesh <priteshrajpurohit85@gmail.com>
2024-12-06 01:09:58 +0530

    Reddis file is  fixed

--- src/app.js
+++ src/app.js
@@ -33,3 +33,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+module.exports = app;
--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,51 +1,45 @@
 const redis = require('redis');
 
 // Create a Redis client with environment variables
-if(process.env.USE_DOCKER){
-  const client = redis.createClient({
-    socket: {
-      host: process.env.REDIS_HOST ,
-      port: process.env.REDIS_PORT 
-    },
-  });
-  
-  console.log("Initializing Redis client...");
-  
-  client.on('error', (err) => {
-    console.error('Redis Client Error:', err);
-  });
-  
-  (async () => {
-    try {
-      await client.connect(); // Explicitly connect the client
-      console.log("Redis client connected successfully");
-    } catch (err) {
-      console.error("Error connecting Redis client:", err);
-    }
-  })();
-  
-  
-  module.exports = client;
+const client = redis.createClient({
+  socket: {
+    host: process.env.REDIS_HOST ,
+    port: process.env.REDIS_PORT 
+  },
+});
 
-}
+console.log("Initializing Redis client...");
+
+client.on('error', (err) => {
+  console.error('Redis Client Error:', err);
+});
+
+(async () => {
+  try {
+    await client.connect(); // Explicitly connect the client
+    console.log("Redis client connected successfully");
+  } catch (err) {
+    console.error("Error connecting Redis client:", err);
+  }
+})();
+
+module.exports = client;
 
 
 
 
 
-//// upr vala use krna  niche ka commentout krdena docker k liye 
+/*
+Use below code for the local environment while the above code is for docker environment
 
+*/
+// const redis = require('redis');
 
-/////////////////////////For direct windows/////////////////////
+// const client = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+// });
+// console.log("redis is running");
+// client.on('error', (err) => console.error('Redis Client Error', err));
 
-else{
-  const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  });
-  console.log("redis is running");
-  client.on('error', (err) => console.error('Redis Client Error', err));
-  module.exports = client;
-}
-
-
+// module.exports = client;
